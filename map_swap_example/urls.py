81e66bbb68256d8d4f6d29cb9925a54c380e2a30
andywar65 <andy.war1965@gmail.com>
2024-04-19 10:36:14 +0200

    redirect to base list

--- map_swap_example/urls.py
+++ map_swap_example/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(pattern_name="locations:base_list")),
     path("location/", include("locations.urls", namespace="locations")),
 ]
