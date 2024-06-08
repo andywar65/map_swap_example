79340c260598e23d0473deab6fbc25d3666c9926
andywar65 <andy.war1965@gmail.com>
2024-06-08 16:59:33 +0200

    adding view tests

--- locations/tests.py
+++ locations/tests.py
@@ -1,6 +1,11 @@
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
+from faker import Faker
+from faker.providers import geo
+
+from .models import Location
 
 pword = settings.DJANGO_SUPERUSER_PASSWORD
 
@@ -10,3 +15,87 @@ class LocationViewTest(TestCase):
     def setUpTestData(cls):
         print("\nTest Location views")
         User.objects.create_user("user", "user@example.com", pword)
+        fake = Faker()
+        fake.add_provider(geo)
+        for i in range(2):
+            loc = fake.location_on_land()
+            Location.objects.create(
+                title=loc[2],
+                description=fake.sentence(),
+                lat=float(loc[0]),
+                long=float(loc[1]),
+                geom={"type": "Point", "coordinates": [float(loc[1]), float(loc[0])]},
+            )
+
+    def test_view_status_no_login(self):
+        print("\n-Test status without logging")
+        response = self.client.get(reverse("locations:base_list"))
+        self.assertEqual(response.status_code, 200)
+        print("\n--Test base list status 200")
+        response = self.client.get(
+            reverse("locations:location_detail", kwargs={"pk": 1})
+        )
+        self.assertEqual(response.status_code, 200)
+        print("\n--Test location detail status 200")
+        response = self.client.get(reverse("locations:location_create"))
+        self.assertEqual(response.status_code, 302)
+        print("\n--Test location create status 302")
+        response = self.client.get(
+            reverse("locations:location_update", kwargs={"pk": 1})
+        )
+        self.assertEqual(response.status_code, 302)
+        print("\n--Test location update status 302")
+        response = self.client.get(
+            reverse("locations:location_delete", kwargs={"pk": 1})
+        )
+        self.assertEqual(response.status_code, 302)
+        print("\n--Test location delete status 302")
+
+    def test_view_status_login(self):
+        print("\n-Test status logged")
+        self.client.login(username="user", password=pword)
+        response = self.client.get(reverse("locations:location_create"))
+        self.assertEqual(response.status_code, 200)
+        print("\n--Test location create status 200")
+        response = self.client.get(
+            reverse("locations:location_update", kwargs={"pk": 1})
+        )
+        self.assertEqual(response.status_code, 200)
+        print("\n--Test location update status 200")
+        response = self.client.get(
+            reverse("locations:location_delete", kwargs={"pk": 1})
+        )
+        self.assertEqual(response.status_code, 404)
+        print("\n--Test location delete status 404")
+
+    def test_view_status_htmx(self):
+        print("\n-Test status with HTMX header")
+        response = self.client.get(
+            reverse("locations:base_list"), headers={"HX-Request": "true"}
+        )
+        self.assertEqual(response.status_code, 200)
+        print("\n--Test with htmx base list status 200")
+        response = self.client.get(
+            reverse("locations:location_detail", kwargs={"pk": 1}),
+            headers={"HX-Request": "true"},
+        )
+        self.assertEqual(response.status_code, 200)
+        print("\n--Test with htmx location detail status 200")
+        self.client.login(username="user", password=pword)
+        response = self.client.get(
+            reverse("locations:location_create"), headers={"HX-Request": "true"}
+        )
+        self.assertEqual(response.status_code, 200)
+        print("\n--Test with htmx location create status 200")
+        response = self.client.get(
+            reverse("locations:location_update", kwargs={"pk": 1}),
+            headers={"HX-Request": "true"},
+        )
+        self.assertEqual(response.status_code, 200)
+        print("\n--Test with htmx location update status 200")
+        response = self.client.get(
+            reverse("locations:location_delete", kwargs={"pk": 1}),
+            headers={"HX-Request": "true"},
+        )
+        self.assertEqual(response.status_code, 200)
+        print("\n--Test with htmx location delete status 200")
